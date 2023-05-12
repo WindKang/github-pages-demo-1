3b6dc6d0653ef643f838a0c9f224fc6dcf6bba65
WindKang <abelfrancis615@gmail.com>
2023-05-13 00:02:51 +0800

    import vue router 4

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { App } from './App';
+import { Bar } from './views/Bar';
+import { Foo } from './views/Foo';
+import {createRouter,createWebHashHistory} from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [  
+  { path: '/', component: Foo },  
+  { path: '/about', component: Bar} ,
+]
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+const app =createApp(App)
+app.use(router)
+app.mount('#app')
