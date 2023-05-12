d03223d64513b36ffd00d9cf723aa20c2022ec95
WindKang <abelfrancis615@gmail.com>
2023-05-12 17:59:23 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-pages-demo-1/dist/',
+//  base: '/github-pages-demo-1/dist/',   //部署到github需要这行代码
   plugins: [vue()]
 })
