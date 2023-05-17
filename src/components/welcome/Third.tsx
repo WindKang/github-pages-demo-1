630693439d84d4add11e92b7cf6bf93dc7caa021
WindKang <abelfrancis615@gmail.com>
2023-05-17 23:06:43 +0800

    使用 SVG Sprites

--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,10 +1,11 @@
 import s from './Welcome.module.scss'
-import chart from '../../assets/icons/chart.svg'
 import { FunctionalComponent } from 'vue';
 export const Third:FunctionalComponent =  () => { 
    return   <div class={s.card}>
-            <img class={s.icon} src={chart} />
-            <h2>每日提醒<br />不遗漏每一笔账单</h2>
+            <svg>
+              <use xlinkHref='#chart'></use>
+             </svg>
+              <h2>每日提醒<br />不遗漏每一笔账单</h2>
             </div>
 }
 
