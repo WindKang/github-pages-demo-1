fba25aa1a1869c87e433cf2addfb1c6c8cfb58da
WindKang <abelfrancis615@gmail.com>
2023-05-17 23:18:17 +0800

    添加useSwipe

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { defineComponent, Transition, VNode } from "vue";
 import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import "./App.scss"
 
-
-export const App = defineComponent({ 
-  setup() {  
+export const App = defineComponent({
+  setup() {
     return () => (
-      <div class="page"> 
-        <RouterView/>
-      </div>  
+      <div class="page">
+        <RouterView />
+      </div>
     )
   }
 })
