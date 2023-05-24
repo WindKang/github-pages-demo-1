b7b2bf360af47e19523ea3921cd77357b64fb58e
WindKang <abelfrancis615@gmail.com>
2023-05-24 21:11:24 +0800

    打算使用grid布局

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -3,7 +3,7 @@ import { Icon } from '../../shared/Icon';
 import s from './InputPad.module.scss';
 import { time } from '../../shared/time';
 //@ts-ignore
-import { Popup,DatetimePicker } from 'vant';
+import { Popup,DatetimePicker} from 'vant';
 export const InputPad = defineComponent({
   props:{
     name:{
@@ -13,25 +13,27 @@ export const InputPad = defineComponent({
   setup: (props,context) => { 
     const now = new Date();
     const refDate = ref<Date>(now);
+    const appendText = (n:number|string) => refAmount.value += n.toString(); 
     const buttons =[
-        {text:'1',onClick: ()=>{}},
-        {text:'2',onClick: ()=>{}},
-        {text:'3',onClick: ()=>{}},
-        {text:'清空',onClick: ()=>{}},
-        {text:'4',onClick: ()=>{}},
-        {text:'5',onClick: ()=>{}},
-        {text:'6',onClick: ()=>{}},
+        {text:'1',onClick: ()=>{appendText(1)}},
+        {text:'2',onClick: ()=>{appendText(2)}},
+        {text:'3',onClick: ()=>{appendText(3)}},
+        {text:'清空',onClick: ()=>{refAmount.value = ''}},
+        {text:'4',onClick: ()=>{appendText(4)}},
+        {text:'5',onClick: ()=>{appendText(5)}},
+        {text:'6',onClick: ()=>{appendText(6)}},
         {text:'+',onClick: ()=>{}},
-        {text:'7',onClick: ()=>{}},
-        {text:'8',onClick: ()=>{}},
-        {text:'9',onClick: ()=>{}},
+        {text:'7',onClick: ()=>{appendText(7)}},
+        {text:'8',onClick: ()=>{appendText(8)}},
+        {text:'9',onClick: ()=>{appendText(9)}},
         {text:'-',onClick: ()=>{}},
         {text:'.',onClick: ()=>{}},
-        {text:'0',onClick: ()=>{}},
+        {text:'0',onClick: ()=>{appendText(0)}},
         {text:'删',onClick: ()=>{}},
         {text:'提交',onClick: ()=>{}},
     ]
-    const refShowPop = ref(false)
+    const refShowPop = ref(false);
+    const refAmount = ref('')
     return () => ( <>
       <div class ={s.dateAndAmount}>
         <span class={s.date}>
@@ -40,12 +42,15 @@ export const InputPad = defineComponent({
           <span onClick={() => refShowPop.value = true}> {time(refDate.value).format()} </span>
             <Popup position='bottom' v-model:show={refShowPop.value}>
               <DatetimePicker v-model={refDate.value} type="date" title="选择年月日"
-                onConfirm={() => refShowPop.value = false} />
+                onConfirm={() => refShowPop.value = false}
+                onCancel={() => refShowPop.value = false}
+                />
             </Popup>
           </span>
         </span>
-        <span class={s.amount}>998.33</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
+
       <div class ={s.buttons}>
         {buttons.map( button =>
           <button onClick ={button.onClick}>{button.text}</button>
