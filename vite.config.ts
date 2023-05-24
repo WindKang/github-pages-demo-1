876da781f8831c5371935947bba5d23d3b435e3c
WindKang <abelfrancis615@gmail.com>
2023-05-24 16:41:29 +0800

    引入 vant 实现日期选择

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from  'vite'
 import vue from   '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
 // @ts-nocheck
 import { svgstore } from './src/vite_plugins/svgstore';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +15,10 @@ export default defineConfig({
       transformOn: true,
       mergeProps:true
     }),
-    svgstore()
+    svgstore(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
   ]
 
 })
