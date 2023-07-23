d8acb0896ee0484806b726a7122b819d187d2544
WindKang <abelfrancis615@gmail.com>
2023-07-23 17:52:04 +0800

    可以编辑标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,37 +1,23 @@
 import { defineComponent,reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Button } from '../../shared/Button';
-import { Icon } from '../../shared/Icon';
-import { Rules, validate } from '../../shared/validate';
 import { TagForm } from './TagForm';
 import s from './Tag.module.scss';
+import { BackIcon } from '../../shared/BackIcon';
+import { useRoute } from 'vue-router';
 export const TagEdit = defineComponent({
-  setup: (props,context) => { 
-    const formData = reactive({
-      name:'',
-      sign:'',
-    })
-    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({}) 
-    //解释  
-    const onSubmit = (e:Event) => {
-       const rules:Rules<typeof formData> =[
-          { key:'name',type:'required' ,message: '必填'}, 
-          { key:'name',type:'pattern',regex: /^.{1,4}$/ ,message: '只能填1-4个字符'},
-          { key:'sign',type:'required',message:'必填'},
-       ]
-       Object.assign(errors, { //这里为啥是Object开头 因为errors是一个reactive对象，所以不能直接赋值，要用Object.assign
-        name: undefined,
-        sign: undefined
-      })
-      Object.assign(errors, validate(formData, rules))
-      e.preventDefault();
+  setup: () => { 
+    const route = useRoute();
+    const numberId = parseInt(route.params.id!.toString())
+    if(Number.isNaN(numberId)){
+      return ()=> <div>id不存在</div>
     }
     return () => (
         <MainLayout >{{
-        title: () => '新增标签',
+        title: () => '编辑标签',
         icon: ()=> <BackIcon/>,
         default :()=> <>
-        <TagForm />
+        <TagForm  id={numberId}/>
         <div class= {s.actions}>
           <Button  level='danger' class ={s.removeTags} onClick ={()=>{}}>删除标签</Button>
           <Button  level='danger' class ={s.removeTagsAndItems} onClick ={()=>{}}>删除标签和记账</Button>
